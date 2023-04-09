26f467210f008de3b8627ee24ac6fead0dfd1568
Hristo Tunchev <126184410+hristotunchev@users.noreply.github.com>
2023-04-09 16:41:54 +0300

    Update Create.js

--- reactjs-project/client/src/components/Create/Create.js
+++ reactjs-project/client/src/components/Create/Create.js
@@ -7,7 +7,7 @@ export default function Create() {
     return (
         <div className="form-container">
             <Form className="create-form">
-                <Form.Label className="form-label">Add new neshto</Form.Label>
+                <Form.Label className="form-label">Add New Vehicle</Form.Label>
 
                 <Form.Group className="mb-3">
                     {/* <Form.Label>Name</Form.Label> */}
@@ -33,7 +33,7 @@ export default function Create() {
                 </Form.Group>
 
                 <Button variant="primary" type="submit" className="submit-btn">
-                    Add neshto
+                    Add Vehicle
                 </Button>
             </Form>
         </div>
