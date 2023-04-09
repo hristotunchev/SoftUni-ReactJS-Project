e56d62326110b1753654524bb2b3128b16a6c52b
Hristo Tunchev <126184410+hristotunchev@users.noreply.github.com>
2023-04-09 18:38:16 +0300

    Update Header.js
    Added navigation conditional rendering.

--- reactjs-project/client/src/components/Header/Header.js
+++ reactjs-project/client/src/components/Header/Header.js
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { AuthContext } from '../../contexts/AuthContext.js';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
 
 import './Header.css';
 
 export default function Header() {
+    const { isAuthenticated, userEmail } = useContext(AuthContext);
+
     return (
         <Navbar className="nav-bar">
             <Container>
@@ -15,10 +20,22 @@ export default function Header() {
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
                         <Nav.Link as={Link} to="/catalog" className="nav-link">Catalog</Nav.Link>
-                        <Nav.Link as={Link} to="/create" className="nav-link">Create</Nav.Link>
-                        <Nav.Link as={Link} to="/login" className="nav-link">Login</Nav.Link>
-                        <Nav.Link as={Link} to="/register" className="nav-link">Register</Nav.Link>
-                        <Nav.Link as={Link} to="/logout" className="nav-link">Logout</Nav.Link>
+                        
+                        {isAuthenticated && (
+                            <>
+                                <Nav.Link as={Link} to="/create" className="nav-link">Create</Nav.Link>
+                                <Nav.Link as={Link} to="/logout" className="nav-link">Logout</Nav.Link>
+                            </>
+                        )}
+
+                        {!isAuthenticated && (
+                            <>
+                                <Nav.Link as={Link} to="/login" className="nav-link">Login</Nav.Link>
+                                <Nav.Link as={Link} to="/register" className="nav-link">Register</Nav.Link>
+                            </>
+                        )}
+
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
