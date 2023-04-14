22a0243d831162a7a96e5d5177fe5ee0c5767e66
Hristo Tunchev <126184410+hristotunchev@users.noreply.github.com>
2023-04-14 17:28:13 +0300

    update catalog

--- reactjs-project/client/src/components/Catalog/Catalog.js
+++ reactjs-project/client/src/components/Catalog/Catalog.js
@@ -9,10 +9,12 @@ export default function Catalog() {
     const { cars } = useContext(CarContext);
 
     return (
-        <Row xs={3} md={4} className="g-4" style={{ margin: '0px 15px 0px 15px' }}>
-            {cars.map(x => <CatalogItem key={x._id} {...x} />)}
-
+        <>
+            <Row xs={3} md={4} className="g-4" style={{ margin: '0px 15px 0px 15px' }}>
+                {cars.map(x => <CatalogItem key={x._id} {...x} />)}
+            </Row>
+            
             {cars.length === 0 && <h3 className="no-cars">No cars yet</h3>}
-        </Row>
+        </>
     );
 };
