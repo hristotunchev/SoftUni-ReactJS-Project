467098bcf50d44ddc333874b5c215d5d902244f0
Hristo Tunchev <126184410+hristotunchev@users.noreply.github.com>
2023-04-17 21:55:38 +0300

    Update Catalog

--- reactjs-project/client/src/components/Catalog/Catalog.js
+++ reactjs-project/client/src/components/Catalog/Catalog.js
@@ -10,11 +10,16 @@ export default function Catalog() {
 
     return (
         <>
-            <Row xs={3} md={4} className="g-4" style={{ margin: '0px 15px 0px 15px' }}>
-                {cars.map(x => <CatalogItem key={x._id} {...x} />)}
-            </Row>
-            
-            {cars.length === 0 && <h3 className="no-cars">No cars yet</h3>}
+            {cars.length !== 0 && (
+                <>
+                <h2 className="all-cars">All Cars</h2>
+                <Row xs={3} md={4} className="g-4" style={{ margin: '0px 15px 0px 15px' }}>
+                    {cars.map(x => <CatalogItem key={x._id} {...x} />)}
+                </Row>
+                </>
+            )}
+
+            {cars.length === 0 && <h3 className="no-cars">No Cars Yet</h3>}
         </>
     );
 };
